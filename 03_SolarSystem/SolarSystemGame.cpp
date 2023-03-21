9309c719c9cc14a0c41da1878b4e9264ff6f0fe3
Сева Мельников <edwardmorra42@gmail.com>
2023-03-21 22:44:39 +0300

    Added new planets

--- 03_SolarSystem/SolarSystemGame.cpp
+++ 03_SolarSystem/SolarSystemGame.cpp
@@ -15,37 +15,112 @@ bool SolarSystemGame::createGameComponents()
 
   mInputDevice->MouseMove.AddLambda([this](const InputDevice::MouseMoveEventArgs& args) { this->processMouseMove(args); });
 
-  mPlanets[0] = new Planet(
-    nullptr, 
-    new CubeRenderItem(mRenderContext, { 1.0, 1.0, 1.0 }, { 1.0, 0.0, 1.0, 1.0 }), 
-    { 0.0, 0.0, 0.0 }, 
-    -1.0, 
-    0.0, 
-    { 0.0, 0.0, 1.0 }, 
-    { 0.0, 0.0, 1.0 });
+  {
+    DirectX::SimpleMath::Vector3 scale = { 1.0f, 1.0f, 1.0f };
+    DirectX::SimpleMath::Vector4 color = { 1.0f, 0.0f, 1.0f, 1.0f };
 
-  mPlanets[1] = new Planet(
-    mPlanets[0],
-    new CubeRenderItem(mRenderContext, { 0.7, 0.7, 0.7 }, { 1.0, 1.0, 0.0, 1.0 }), 
-    { 0.0, 5.0, 0.0 }, 
-    0.0, 
-    3.0, 
-    { 0.0, 1.0, 0.0 }, 
-    { 0.0, 0.0, 1.0 });
+    DirectX::SimpleMath::Vector3 initialPosition = { 0.0, 0.0, 0.0 };
+    float localRotationSpeed = -1.0;
+    float parentRotationSpeed = 0.0;
+    DirectX::SimpleMath::Vector3 localRotationAxis = { 0.0, 0.0, 1.0 };
+    DirectX::SimpleMath::Vector3 parentRotationAxis = { 0.0, 0.0, 1.0 };
 
-  mPlanets[2] = new Planet(
-    mPlanets[1],
-    new CubeRenderItem(mRenderContext, { 0.3, 0.3, 0.3 }, { 1.0, 0.0, 0.0, 1.0 }), 
-    { 2.0, 0.0, 0.0 }, 
-    0.0, 
-    3.0, 
-    { 0.0, 1.0, 0.0 }, 
-    { 0.0, 1.0, 0.0 });
+    mPlanets[0] = new Planet(
+      nullptr,
+      new CubeRenderItem(mRenderContext, scale, color),
+      initialPosition,
+      localRotationSpeed,
+      parentRotationSpeed,
+      localRotationAxis,
+      parentRotationAxis);
+  }
+
+  {    
+    DirectX::SimpleMath::Vector3 scale = { 0.7f, 0.7f, 0.7f };
+    DirectX::SimpleMath::Vector4 color = { 1.0f, 1.0f, 0.0f, 1.0f };
+
+    DirectX::SimpleMath::Vector3 initialPosition = { 0.0, 5.0, 0.0 };
+    float localRotationSpeed = 0.0;
+    float parentRotationSpeed = 3.0;
+    DirectX::SimpleMath::Vector3 localRotationAxis = { 0.0, 0.0, 1.0 };
+    DirectX::SimpleMath::Vector3 parentRotationAxis = { 0.0, 0.0, 1.0 };
+
+    mPlanets[1] = new Planet(
+      mPlanets[0],
+      new CubeRenderItem(mRenderContext, scale, color),
+      initialPosition,
+      localRotationSpeed,
+      parentRotationSpeed,
+      localRotationAxis,
+      parentRotationAxis);
+  }
+
+  {    
+    DirectX::SimpleMath::Vector3 scale = { 0.3f, 0.3f, 0.3f };
+    DirectX::SimpleMath::Vector4 color = { 1.0f, 0.0f, 0.0f, 1.0f };
+
+    DirectX::SimpleMath::Vector3 initialPosition = { 0.0, 0.0, 2.0 };
+    float localRotationSpeed = 0.0;
+    float parentRotationSpeed = 3.0;
+    DirectX::SimpleMath::Vector3 localRotationAxis = { 0.0, 0.0, 1.0 };
+    DirectX::SimpleMath::Vector3 parentRotationAxis = { 0.0, 1.0, 0.0 };
+
+    mPlanets[2] = new Planet(
+      mPlanets[1],
+      new CubeRenderItem(mRenderContext, scale, color),
+      initialPosition,
+      localRotationSpeed,
+      parentRotationSpeed,
+      localRotationAxis,
+      parentRotationAxis);
+  }
+
+  {    
+    DirectX::SimpleMath::Vector3 scale = { 0.4f, 0.2f, 0.1f };
+    DirectX::SimpleMath::Vector4 color = { 0.5f, 0.3f, 0.1f, 1.0f };
+
+    DirectX::SimpleMath::Vector3 initialPosition = { 8.0, 0.0, 0.0 };
+    float localRotationSpeed = 6.0;
+    float parentRotationSpeed = 4.0;
+    DirectX::SimpleMath::Vector3 localRotationAxis = { 0.0, 1.0, 1.0 };
+    DirectX::SimpleMath::Vector3 parentRotationAxis = { 0.0, -1.0, 1.0 };
+
+    mPlanets[3] = new Planet(
+      mPlanets[0],
+      new CubeRenderItem(mRenderContext, scale, color),
+      initialPosition,
+      localRotationSpeed,
+      parentRotationSpeed,
+      localRotationAxis,
+      parentRotationAxis);
+  }
+
+  {    
+    DirectX::SimpleMath::Vector3 scale = { 0.1f, 0.1f, 0.1f };
+    DirectX::SimpleMath::Vector4 color = { 1.0f, 0.0f, 0.1f, 1.0f };
+
+    DirectX::SimpleMath::Vector3 initialPosition = { 1.0, 0.0, 0.0 };
+    float localRotationSpeed = 0.0;
+    float parentRotationSpeed = 2.0;
+    DirectX::SimpleMath::Vector3 localRotationAxis = { 0.0, 1.0, 1.0 };
+    DirectX::SimpleMath::Vector3 parentRotationAxis = { 0.0, 0.0, -1.0 };
+
+    mPlanets[4] = new Planet(
+      mPlanets[3],
+      new CubeRenderItem(mRenderContext, scale, color),
+      initialPosition,
+      localRotationSpeed,
+      parentRotationSpeed,
+      localRotationAxis,
+      parentRotationAxis);
+  }
 
   mGameComponents.push_back(mCamera);
   mGameComponents.push_back(mPlanets[0]);
   mGameComponents.push_back(mPlanets[1]);
   mGameComponents.push_back(mPlanets[2]);
+  mGameComponents.push_back(mPlanets[3]);
+  mGameComponents.push_back(mPlanets[4]);
 
   return true;
 }
